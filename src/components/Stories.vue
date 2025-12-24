0eed271eaf1a43b1c40c8df3910cadefec860145
al-kawsar <tlod9906@gmail.com>
2025-12-24 22:22:38 +0800

    feat: split story code to new component

---
+++ src/components/Stories.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+<template>
+  <section class="stories-section">
+    <div class="stories-scroll">
+      <!-- My Story -->
+      <div class="story-item">
+        <div class="avatar-ring my-story">
+          <img
+            src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=200&auto=format&fit=crop"
+            class="story-avatar"
+          />
+          <div class="add-badge">+</div>
+        </div>
+        <span class="story-name" style="color: var(--text-secondary)"
+          >Cerita Anda</span
+        >
+      </div>
+      <!-- Friend 1 -->
+      <div class="story-item">
+        <div class="avatar-ring">
+          <img
+            src="https://images.unsplash.com/photo-1517841905240-472988babdf9?q=80&w=200&auto=format&fit=crop"
+            class="story-avatar"
+          />
+        </div>
+        <span class="story-name">sarah_w</span>
+      </div>
+    </div>
+  </section>
+</template>
